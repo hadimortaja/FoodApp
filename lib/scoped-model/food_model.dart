5a2dc0f49510d646064037f6a3f1037f8cc9c35b
hadi <hadimortaja9@gmail.com>
2020-08-21 00:29:15 +0300

    FetchingDataFromPHPMySQLAPIUsinghttpPackage

--- lib/scoped-model/food_model.dart
+++ lib/scoped-model/food_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:food_delivery_app/models/food_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:http/http.dart' as http;
@@ -14,8 +16,25 @@ class FoodModel extends Model {
   }
 
   void fetchFoods() {
-    http.get("http://192.168.56.1/flutter_food_app/api/foods/getfoods.php").then((http.Response response) {
-        print("Fetching Data : ${response.body}");
+    http.get("http://192.168.1.10/flutter_food_app/api/foods/getfoods.php").then((http.Response response) {
+      //  print("Fetching Data : ${response.body}");
+        final List fetchedData = json.decode(response.body);
+        final List<Food> fetchedFoodItems =[];
+        //print(fetchedData);
+        fetchedData.forEach((data) {
+          Food food =Food(
+            id: data["id"],
+            category: data["category"],
+            discount: double.parse(data["discount"]),
+            imagePath: data["imagePath"],
+            name: data["name"],
+            price: double.parse(data["price"]),
+          );
+          fetchedFoodItems.add(food);
+         });
+         _foods =fetchedFoodItems;
+             print(_foods);
+
     });
   }
 }
